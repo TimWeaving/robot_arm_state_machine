be2b075c50598b098ec9b6cde8ce74b67fd5b3f3
Evan Sutcliffe <30503110+evansutcliffe@users.noreply.github.com>
2020-10-21 18:25:38 +0100

    Cleaned up and added joint defination idea

---
+++ joints.py
@@ -0,0 +1,13 @@
+
+
+def joint_0(x,y,z):
+    # do some function to transfer movement of joint into movement in global axis
+    # depends on oriention of all other joints so not sure how this is done off the top of my head
+    x_dash,y_dash,z_dash = x,y,z
+    return x_dash,y_dash,z_dash
+
+def joint_1(x,y,z):
+    # do some function to transfer movement of joint into movement in global axis
+    # depends on oriention of all other joints so not sure how this is done off the top of my head
+    x_dash,y_dash,z_dash = x,y,z
+    return x_dash,y_dash,z_dash
--- state_machine.py
+++ state_machine.py
@@ -1,8 +1,7 @@
 
 import time
-
 from enum import Enum
-
+import threading
 
 class State(Enum):
     startup = 1
@@ -15,14 +14,16 @@ class joint:
     def __init__(self,joint_num):
         self.type = joint_num    # instance variable unique to each instance
         self.name = "joint"+str(self.type)
-        self.jobs = ['left','right','africa by toto']
-        self.state = State.startup
+        self.jobs = []#['left','right','help me','africa by toto']
         self.state = State.startup
         #ser = Serial()
         self.ser = object()
+        self.startup()
+        
+
 
     def is_ready(self):
-        return self.state== State.waiting
+        return self.state == State.waiting
     
     def set_finished(self):
         self.state = State.shutdown
@@ -37,89 +38,73 @@ class joint:
         return self.jobs
     
     def startup(self):
-        self.serial_state_machine_startup(self.ser,self.jobs)
+         time.sleep(0.5)
+         x = threading.Thread(target=self.serial_state_machine_startup)
+         x.start()
+         #x.join()
         
-    
-    #self.serial_state_machine_startup()
+        
+
+    def serial_state_machine_startup(self):
+        
+        # do_startup
+        while (self.state == State.startup):
+            if (self.startup_successful()):
+                self.state = State.waiting
+            else:
+                self.state = State.shutdown
+            
+        if (self.state == State.waiting):
+            self.serial_state_machine_waiting()
+            
+        elif(self.state == State.shutdown):
+             self.serial_state_machine_shutdown()   
     
 
-    def serial_state_machine_startup(self,ser,jobs):
-        state = self.state
-        # do_startup
-        while (state == State.startup):
-            if (self.startup_successful()):
-                state = State.waiting
-            else:
-                state = State.shutdown
+    def serial_state_machine_waiting(self):
+        while (self.state == State.waiting):
+            if (len(self.get_jobs())> 0 ):
+                self.state = State.write     
             
-        if (state == State.waiting):
-            self.serial_state_machine_waiting(state,ser,jobs)
+        if (self.state == State.write):
+            self.serial_state_machine_write()
             
-        elif(state == State.shutdown):
-             self.serial_state_machine_shutdown(state,ser,jobs)   
+        elif(self.state == State.shutdown):
+             self.serial_state_machine_shutdown()   
+        
     
-    def serial_state_machine_waiting(self,state,ser,jobs):
-        start = time.time()
-        print("write:")
-        while (state == State.waiting):
-            jobs = self.get_jobs()
-            if (len(jobs)>0):
-                state = State.write     
-            if (self.get_finished()or (time.time()-start)>2.0):
-                state = State.shutdown
-            
-        if (state == State.write):
-            self.serial_state_machine_write(state,ser,jobs)
-            
-        elif(state == State.shutdown):
-             self.serial_state_machine_shutdown(state,ser,jobs)   
-        return 0 
-    
-    def serial_state_machine_write(self,state,ser,jobs):
+    def serial_state_machine_write(self):
+        #ser = self.ser
         # will wait in this state until it gets a command
-        print("write:")
-        job = jobs.pop(0)
-        job_done = False
-        while (state == State.write and not job_done):
-            time.sleep(1)
-            print(job)
-            job_done = True
+
+        #jobs = self.get_jobs()
+        job = self.get_jobs().pop(0)
+        job_finished = False
+        while (self.state == State.write and not job_finished):
+            print("thread"+self.name+": move"+str(job))
+            time.sleep(job/1000) # pretend job takes as long as its number
+            job_finished = True
             # compile job into required format
             # send job over serial to robot arm
             # confirm job has been completed
+            
         
-        if(state == State.shutdown):
-            self.serial_state_machine_shutdown(state,ser,jobs)
-        elif(len(jobs) == 0):
-            state = State.waiting
-            self.serial_state_machine_waiting(state,ser,jobs)
-        elif (len(jobs)>0):
-            self.serial_state_machine_write(state,ser,jobs)        
+        if(self.state == State.shutdown):
+            self.state = State.shutdown
+            self.serial_state_machine_shutdown()
+        elif (len(self.get_jobs())>0):
+            self.state = State.write
+            self.serial_state_machine_write()       
+        elif(len(self.get_jobs()) == 0):
+            self.state = State.waiting
+            self.serial_state_machine_waiting() 
     
     
-    def serial_state_machine_shutdown(self,state,ser,jobs):
+    def serial_state_machine_shutdown(self):
         #ser.close()
-        print("shutdown complete")
         return 0
         # maybe return arm to starting point 
     
     def startup_successful(self):
         return True
-    
-a = joint(0)
-#a.update_jobs(["do a new job"])
-a.startup()
-time.sleep(1)
-a.set_finished()
-
-print(a.get_finished())
-'''
-new_jobs = ['startup','move up', 'move left', 'move claw','play africa by toto']
-finished = False
-#serial_state_machine_generic(0)
-serial_state_machine_startup()
-time.sleep(3)
-new_jobs = ['another job']
-finished = True
-
-'''
+   
